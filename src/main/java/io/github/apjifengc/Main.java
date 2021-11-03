375ddc3a77449a04ae21e7db2c71d67a49e68da1
APJifengc <mcbigshen@qq.com>
2021-11-03 09:11:36 +0800

    :sparkles: Argument parse.

--- src/main/java/io/github/apjifengc/Main.java
+++ src/main/java/io/github/apjifengc/Main.java
@@ -1,8 +1,116 @@
 package io.github.apjifengc;
 
+import lombok.Getter;
+
+import java.io.*;
+import java.util.*;
+
 public class Main {
+    private static final String USAGE = "Usage: <file> [-o <output_file>] [-d]";
+    private static final String HELP = """
+                <file>   Select the input file.
+            -h, --help   Show this help message.
+            -o, --output Select the output file.
+            -d, --debug  Print the debug info.""";
+    public static final PrintStream OUTPUT = System.out;
+    public static final PrintStream ERROR = System.err;
+    @Getter
+    private static File inputFile;
+    @Getter
+    private static File outputFile;
+    @Getter
+    private static Scanner input;
+    @Getter
+    private static PrintStream output;
+    @Getter
+    private static boolean debugMode = false;
+
+    private static void parseArguments(String[] args) {
+        Queue<String> argsQueue = new ArrayDeque<>(Arrays.asList(args));
+        if (argsQueue.isEmpty()) {
+            OUTPUT.println(USAGE);
+
+            System.exit(0);
+        }
+        while (!argsQueue.isEmpty()) {
+            String argType = argsQueue.poll();
+            switch (argType) {
+                case "-h":
+                case "--help":
+                    OUTPUT.println(USAGE);
+                    OUTPUT.println(HELP);
+                    System.exit(0);
+                case "-o":
+                case "--output":
+                    if (argsQueue.isEmpty()) {
+                        ERROR.println("Error: '-o' expects output file name.");
+                        System.exit(-1);
+                    }
+                    if (outputFile != null) {
+                        ERROR.println("Error: Cannot use more than 1 '-o' argument.");
+                        System.exit(-1);
+                    }
+                    try {
+                        outputFile = new File(argsQueue.peek());
+                        output = new PrintStream(outputFile);
+                    } catch (IOException e) {
+                        ERROR.printf("Error: Cannot open the output file '%s'.\n", argsQueue.peek());
+                        e.printStackTrace();
+                        System.exit(-1);
+                    }
+                    argsQueue.remove();
+                    break;
+                case "-d":
+                case "--debug":
+                    debugMode = true;
+                    break;
+                default:
+                    if (argType.startsWith("-")) {
+                        ERROR.printf("Error: Unknown argument '%s'.\n", argType);
+                        System.exit(-1);
+                    } else {
+                        try {
+                            inputFile = new File(argType);
+                            input = new Scanner(inputFile);
+                        } catch (FileNotFoundException e) {
+                            ERROR.printf("Error: Cannot open the input file '%s'.\n", argType);
+                            e.printStackTrace();
+                            System.exit(-1);
+                        }
+                    }
+            }
+        }
+        if (outputFile == null) {
+            int pos = inputFile.getName().lastIndexOf('.');
+            if (pos == -1) {
+                outputFile = new File(inputFile + "_optimized");
+            } else {
+                String fileName = inputFile.getName().substring(0, pos);
+                String fileSuffix = inputFile.getName().substring(pos);
+                outputFile = new File(fileName + "_optimized" + fileSuffix);
+            }
+            try {
+                output = new PrintStream(outputFile);
+            } catch (FileNotFoundException e) {
+                ERROR.printf("Error: Cannot open the output file '%s'.\n", outputFile);
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
+    }
+
+    public static void log(String info, Object... args) {
+        if (debugMode) {
+            OUTPUT.printf(info + "\n", args);
+        }
+    }
 
     public static void main(String[] args) {
-
+        parseArguments(args);
+        log("Start parsing...");
+        while (input.hasNextLine()) {
+            output.println(input.nextLine());
+        }
+        log("Output done.");
     }
 }
